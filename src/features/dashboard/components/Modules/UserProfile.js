76d5a1787760e179dd9e824dedb1752b9777e527
gchakraborty1 <thegourabchakraborty@gmail.com>
2025-12-16 21:11:43 +0530

    chore: stop tracking generated .js files alongside TSX sources

--- src/features/dashboard/components/Modules/UserProfile.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-const UserProfile = () => {
-    return (_jsxs("div", { children: [_jsx("h1", { children: "User Profile" }), _jsx("p", { children: "This is the user profile module." })] }));
-};
-export default UserProfile;
