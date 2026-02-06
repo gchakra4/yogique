bfd753d714ee1d47524a98ecc4f8bb48e113c261
gchakra4 <ext.referral@gmail.com>
2026-02-06 14:23:15 +0530

    Updated package-lock.json

--- src/shared/components/ui/Button.test.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { render, screen } from '@testing-library/react';
-import { Button } from './Button';
-test('renders button with children', () => {
-    render(_jsx(Button, { children: "Click me" }));
-    const buttonElement = screen.getByText(/Click me/i);
-    expect(buttonElement).toBeInTheDocument();
-});
